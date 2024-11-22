9b82d4ac3a390f72a6d8eddb63c5228b7021b571
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-11-22 18:06:31 -0500

    feat(): create close connection method

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.apache.log4j.Logger;
 
 public class Account {
@@ -7,27 +9,34 @@ public class Account {
     private static final String URL = "jdbc:mysql://localhost:3306/account";
     private static final String USER = "root";
     private static final String PASS = "rootroot";
+    private static Connection connection = null;
 
     public static void main(String[] args) throws Exception {
-        Connection connection = null;
         try {
             connection = getConnection();
             logger.info("Connection successful with DB");
         } catch (Exception e) {
-            e.printStackTrace();
             logger.error("Connection failure with DB");
+            throw new Exception(e);
         } finally {
-            try {
-                connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                logger.error("Error closing connection");
-            }
+            closeConnection();
         }
     }
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASS);
+    }
+
+    public static void closeConnection() throws Exception {
+        if (connection != null && !connection.isClosed()) {
+            try {
+                connection.close();
+                logger.info("Connection Closed Successful");
+            } catch (Exception e) {
+                logger.error("Error closing connection");
+                throw new Exception(e);
+            }
+        }
     }
 }
